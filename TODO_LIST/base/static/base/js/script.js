c597a40c4ad5aaefd92ef16772837d25bb924c25
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-05-25 19:26:11 +0530

    update 'TODO_LIST'

--- TODO_LIST/base/static/base/js/script.js
+++ TODO_LIST/base/static/base/js/script.js
@@ -20,3 +20,45 @@ const themeToggler = (event) => {
 
 //* Event Listners
 themeBtn.addEventListener('click', themeToggler, false)
+
+
+
+
+//! GSAP code
+gsap.registerPlugin(TextPlugin)
+
+
+gsap.to(".welcome-heading", {
+duration: 3,
+// color: "greenyellow",
+// textDecoration: "underline blue",
+// delay: 3,
+repeat: -1,
+yoyo:true,
+text : "Welcome...."
+})
+
+gsap.from(".todo-header", {
+  // x: 200,
+  y: -15,
+  duration: 1,
+  // delay: 1,
+  ease: "power1.out",
+  opacity: 0,
+})
+gsap.from(".todo-image", {
+  x: 50,
+  // y: 200,
+  duration: 3,
+  delay: 1,
+  ease: "power1.out",
+  opacity: 0,
+})
+gsap.from(".welcome-title", {
+  x: -50,
+  // y: -200,
+  duration: 2,
+  delay: 1,
+  ease: "power1.out",
+  opacity: 0,
+})
