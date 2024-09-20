d040ec6f1eee6cabf92ec17b50a5bc878cc268c1
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-09-20 19:56:12 +0530

    chore: build packages for deployments

---
+++ TODO_LIST/TODO_LIST/base/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .views import home, TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, UserRegisterView, UserLoginView, Index
+from django.contrib.auth.views import LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('register/', UserRegisterView.as_view(), name='signup'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+    path('password_reset/', PasswordResetView.as_view(template_name='base/pswd_reset.html'), name='pswd_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(template_name='base/pswd_reset_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>', PasswordResetConfirmView.as_view(template_name='base/pswd_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_complete/', PasswordResetCompleteView.as_view(template_name='base/pswd_reset_complete.html'), name='password_reset_complete'),
+    path('tasks/', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>', TaskDetail.as_view(), name='task'),
+    path('task_create/', TaskCreate.as_view(), name='task_create'),
+    path('task_update/<int:pk>', TaskUpdate.as_view(), name='task_update'),
+    path('task_delete/<int:pk>', TaskDelete.as_view(), name='task_delete'),
+    path('index/', Index.as_view(), name='index')
+]
---
+++ TODO_LIST/TODO_LIST/base/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.views.generic import TemplateView
+
+from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import login
+
+from .models import Task, User
+from .forms import UserSignupForm
+
+
+# Create your views here.
+def home(request):
+  context = {}
+  return render(request, 'base/home.html', context=context)
+
+class UserRegisterView(FormView):
+  template_name = 'base/signup.html'
+  form_class = UserSignupForm
+  redirect_authenticated_user = True
+
+  success_url = reverse_lazy('tasks')
+
+  def form_valid(self, form):
+    user = form.save()
+    if user is not None:
+      login(self.request, user)
+    return super(UserRegisterView, self).form_valid(form)
+  
+  def get(self, *args, **kwargs):
+    if self.request.user.is_authenticated:
+      return redirect('tasks')
+    return super(UserRegisterView, self).get(*args, **kwargs)
+
+
+class UserLoginView(LoginView):
+  template_name = 'base/login.html'
+  fields = '__all__'
+  redirect_authenticated_user = True
+
+  def get_success_url(self) -> str:
+    return  reverse_lazy('tasks')
+
+
+class TaskList(LoginRequiredMixin, ListView):
+  model = Task
+  context_object_name = 'tasks'
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['tasks'] = context['tasks'].filter(user=self.request.user)
+    context['count'] = context['tasks'].filter(complete=False)
+
+    search_input = self.request.GET.get('search-area') or ''
+    if search_input:
+      context['tasks'] = context['tasks'].filter(title__icontains=search_input)
+    context['search_input'] = search_input
+    
+    return context
+
+class TaskDetail(LoginRequiredMixin, DetailView):
+  model = Task
+  context_object_name = 'task'
+
+class TaskCreate(LoginRequiredMixin, CreateView):
+  model = Task
+  fields = ['title', 'description']
+  success_url = reverse_lazy('tasks')
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super(TaskCreate, self).form_valid(form)
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
+  model = Task
+  fields = ['title', 'description', 'complete']
+  success_url = reverse_lazy('tasks')
+
+class TaskDelete(LoginRequiredMixin, DeleteView):
+  model = Task
+  success_url = reverse_lazy('tasks')
+  
+  
+
+class Index(TemplateView):
+  template_name = 'base/home.html'
+
+  def get_context_data(self, **kwargs):
+    greet = "🙏🏻 !! ...Jay Jay Shree Radhe Krushna... !! 🙏🏻"
+    name = "Shree Raghav Suryavanshi"
+    city = 'Ayodhya'
+    spouse = 'Shree Janaki ji'
+
+    context = {'name':name, 'greet':greet, 'city':city, 'spouse':spouse}
+
+    return context
